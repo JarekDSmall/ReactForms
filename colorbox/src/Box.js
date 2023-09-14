f91a2c813387fafdc2260bc436ec24802d6af6c4
JarekDSmall <129123790+JarekDSmall@users.noreply.github.com>
2023-09-13 23:27:37 -0400

    Implemented the Box Component
    Created the Box component to display individual boxes with specified dimensions and colors.
    Added a remove button to each box to allow for its removal.

---
+++ colorbox/src/Box.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Box({ id, width, height, backgroundColor, removeBox }) {
+  return (
+    <div style={{ width: `${width}px`, height: `${height}px`, backgroundColor: backgroundColor }}>
+      <button onClick={() => removeBox(id)}>X</button>
+    </div>
+  );
+}
+
+export default Box;
